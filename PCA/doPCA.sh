6018ddc379a9f73b14efd59ea914975d74f70f0c
ikremsky <ikremsky@gmail.com>
2023-07-21 15:10:06 -0700

    Update doPCA.sh

--- PCA/doPCA.sh
+++ PCA/doPCA.sh
@@ -6,10 +6,7 @@ x=""
 for celltype in CA DG FCortex RCortex
 do
 	x=$(echo $x" "${celltype}_GC_SAL ${celltype}_FLT_SAL)
-#	echo $x
-#	Rscript /home/samir/Vmao_Nanopore/PCA.R ${celltype} ${celltype}_GC_SAL ${celltype}_FLT_SAL &
 done
-wait
 echo $x
 Rscript /home/samir/Vmao_Nanopore/PCA_All_Reps.R $x
 echo done
